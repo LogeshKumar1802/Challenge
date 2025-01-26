580d5254a08e76a59880b55ddc4555e145aa7192
LogeshKumar1802 <logeshkumar1898@gmail.com>
2025-01-26 13:10:42 +0530

    String repetition problem

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
-﻿namespace Challenge;
+﻿using System.Text;
+namespace Challenge;
 internal class Program {
     static void Main (string[] args) {
+        Console.Write ("Enter the input: ");
+        var ip = Console.ReadLine ()?.ToLower ();
+        if (string.IsNullOrWhiteSpace (ip)) return;
+        StringBuilder sb = new ();
+        char c = ' ';
+        for (int i = 0; i < ip!.Length; i++) {
+            if (ip![i] >= 'a' && ip![i] <= 'z') {
+                Print (c);
+                c = ip[i];
+            } else if (ip![i] >= '0' && ip![i] <= '9') sb.Append (ip[i]);
+            else Print (c);
+        }
+        if (sb != null) Print (c);
 
+        void Print (char ch) {
+            if (int.TryParse (sb.ToString (), out int count)) {
+                Console.Write (new string (ch, count));
+                sb.Clear ();
+            }
+        }
     }
 }
